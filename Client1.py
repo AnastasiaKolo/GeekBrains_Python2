176cf349faaa2e4a7998eb13dd208a6888e9f1d1
AnastasiaKolo <kolodochko@gmail.com>
2018-12-26 02:26:58 +0300

    трудности

--- Client1.py
+++ Client1.py
@@ -1,17 +1,41 @@
+# 1. Реализовать простое клиент-серверное взаимодействие
+# по протоколу JIM (JSON instant messaging):
+# клиент отправляет запрос серверу;
+# сервер отвечает соответствующим кодом результата.
+# Клиент и сервер должны быть реализованы в виде
+# отдельных скриптов, содержащих соответствующие функции.
+# Функции клиента: сформировать presence-сообщение;
+# отправить сообщение серверу;
+# получить ответ сервера;
+# разобрать сообщение сервера;
+# параметры командной строки скрипта
+# client.py <addr> [<port>]:
+# addr — ip-адрес сервера;
+# port — tcp-порт на сервере, по умолчанию 7777.
+
 from socket import *
 import time
 
-
+port = 7777
+host = 'localhost'
 s = socket(AF_INET, SOCK_STREAM)
-s.connect(('localhost', 8888))
-tm = s.recv(1024)
-s.close()
-print("Текущее время: %s" % tm.decode('ascii'))
+try:
+    s.connect((host, port))
+except ConnectionRefusedError:
+    print("Сервер %s недоступен по порту %s" % (host, port))
+msg = 'Привет, сервер'
+s.send(msg.encode('utf-8'))
+data = s.recv(1000000)
+print("Сообщение от сервера: ", data.decode('utf-8'), ' длиной ', len(data), ' байт')
+msg = 'ЙЦУКЕНГШЩЩ'
+s.send(msg.encode('utf-8'))
+data = s.recv(1000000)
+print("Сообщение от сервера: ", data.decode('utf-8'), ' длиной ', len(data), ' байт')
+
 # в цикле
 for i in range(5):
-    s = socket(AF_INET, SOCK_STREAM)
-    s.connect(('localhost', 8888))
-    tm = s.recv(1024)
-    s.close()
-    print("Текущее время: %s" % tm.decode('ascii'))
+    s.send(msg.encode('utf-8'))
+    data = s.recv(1000000)
+    print("Сообщение от сервера: ", data.decode('utf-8'), ' длиной ', len(data), ' байт')
     time.sleep(i)
+s.close()
--- Server1.py
+++ Server1.py
@@ -1,14 +1,27 @@
+# Функции сервера:
+# принимает сообщение клиента;
+# формирует ответ клиенту;
+# отправляет ответ клиенту;
+# имеет параметры командной строки:
+# -p <port> — TCP-порт для работы
+# (по умолчанию использует 7777);
+# -a <addr> — IP-адрес для прослушивания
+# (по умолчанию слушает все доступные адреса).
+
 from socket import *
 import time
 import json
 
+port = 7777
 s = socket(AF_INET, SOCK_STREAM)
-s.bind(('' , 8888))
+s.bind(('' , port))
 s.listen( 5 )
 
 while True:
     client, addr = s.accept()
     print("Получен запрос на соединение от %s" % str(addr))
-    timestr = time.ctime(time.time())+ "\n"
-    client.send(timestr.encode('ascii'))
+    data = client.recv(1000000)
+    print("Сообщение", data.decode('utf-8'), ", было отправлено клиентом: %s" % str(addr))
+    msg = "Привет ! Время "+ time.ctime(time.time())
+    client.send(msg.encode('utf-8'))
     client.close()
